3dfe2e50c51973de6473015e2df3fa2254367862
AymanRamadeen <ayman.abd2288@gmail.com>
2021-02-14 01:57:01 +0200

    files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let qQ1=prompt('what is your name');
+alert('your name is'+qQ1 );
+console.log(qQ1);
+let qQ2=prompt('what is your favorit color');
+alert('your favorit color is '+qQ2);
+console.log(qQ2);
+let qQ3=prompt('what is your favorit hobby');
+alert('your hobby is'+ qQ3);
+console.log(qQ3);
+let qQ4=prompt('what is the color of sky');
+alert('the color of sky is'+qQ4);
+console.log(qQ4);
